1eacb2372c64089930d17599656ebbed7598761e
jigga byte <jigga.e10@gmail.com>
2025-06-25 16:25:03 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+module.exports = function (eleventyConfig) {
+  /* 1. RSS */
+  eleventyConfig.addPlugin(pluginRss);
+
+  /* 2. Blog collection */
+  eleventyConfig.addCollection("posts", (col) =>
+    col.getFilteredByGlob("src/posts/**/*.md").reverse()
+  );
+
+  /* 3. Absolute URL helper for feeds/emails */
+  eleventyConfig.addFilter("abs", (url, site) =>
+    pluginRss.absoluteUrl(url, site.url)
+  );
+
+  /* 4. Global data */
+  return {
+    dir: { input: "src", output: "dist" },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    pathPrefix: "/",            // change if you deploy to /repo-name
+  };
+};
